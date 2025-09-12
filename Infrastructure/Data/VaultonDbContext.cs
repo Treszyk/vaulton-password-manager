0109d62779a892ee3e597b7d503ff84bec3d4cf5
Patryk <79578683+Treszyk@users.noreply.github.com>
2025-09-13 01:14:46 +0200

    feat(infra): add VaultonDbContext with Users and Entries

---
+++ Infrastructure/Data/VaultonDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+	public class VaultonDbContext(DbContextOptions<VaultonDbContext> options) : DbContext(options)
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Entry> Entries {  get; set; }
+	}
+}
