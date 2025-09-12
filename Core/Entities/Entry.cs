7e751f15e80e3b08e96572fdbf01c77a80067e5c
Patryk <79578683+Treszyk@users.noreply.github.com>
2025-09-12 03:15:36 +0200

    feat(core): add User and Entry entities

---
+++ Core/Entities/Entry.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Entities;
+
+public class Entry
+{
+	public Guid Id { get; set; }
+	public Guid UserId { get; set; }
+
+	// Encryption fields
+	public byte[] Nonce { get; set; } = default!;       // random value that makes each encryption unique
+	public byte[] CipherText { get; set; } = default!;  // Encrypted payload, encrypt-all
+	public byte[] Tag { get; set; } = default!;         // Auth tag, to make sure the payload wasnt tampered with since last encryption
+
+	// metadata
+	public DateTime CreatedAt { get; set; }
+	public DateTime UpdatedAt { get; set; }
+}
---
+++ Core/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Entities;
+
+public class User
+{
+	public Guid Id { get; set; }
+
+	// Auth
+	public required byte[] Verifier { get; set; }    // Argon2id hash of the login password
+	public required byte[] S_Pwd { get; set; }       // Salt used with Argon2id for the password
+
+	// Recovery Key - user can opt in for recovery, might change it to forced
+	public byte[]? S_Rk { get; set; }                // Salt used with Argon2id for the recovery Key
+	public byte[]? MK_Wrap_Rk { get; set; }          // Master Key encrypted with the recovery Key
+
+	public required byte[] MK_Wrap_Pwd { get; set; } // MK wrapped/encrypted with the password
+
+	// metadata
+	public DateTime CreatedAt { get; set; }
+	public DateTime UpdatedAt { get; set; }
+	public DateTime? LastLoginAt { get; set; }
+}
