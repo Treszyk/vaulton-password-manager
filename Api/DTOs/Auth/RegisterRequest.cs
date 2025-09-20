c762d47dad49cc29d541d5d2eba6cb939bb92ddf
Patryk <79578683+Treszyk@users.noreply.github.com>
2025-09-21 00:52:20 +0200

    feat(auth): include S_Verifier in RegisterRequest

--- Api/DTOs/Auth/RegisterRequest.cs
+++ Api/DTOs/Auth/RegisterRequest.cs
@@ -5,10 +5,11 @@ namespace Api.DTOs.Auth;
 
 public sealed record RegisterRequest(
 	[Required][property: JsonPropertyName("Verifier")] byte[] Verifier,
+	[Required][property: JsonPropertyName("S_Verifier")] byte[] S_Verifier,
 	[Required][property: JsonPropertyName("S_Pwd")] byte[] S_Pwd,
 	[Required][property: JsonPropertyName("MK_Wrap_Pwd")] byte[] MK_Wrap_Pwd,
 
-	// optional RK things (for now)
+	// optional RK things (I'm gonna be making these mandatory later)
 	[property: JsonPropertyName("S_Rk")] byte[]? S_Rk,
 	[property: JsonPropertyName("MK_Wrap_Rk")] byte[]? MK_Wrap_Rk
 );
