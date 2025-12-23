3deb344938b2c13ba9740f4357ce356ea6f87440
treszyk <79578683+Treszyk@users.noreply.github.com>
2025-12-23 11:48:31 +0100

    refactor(db): reorganize VaultonDbContext, add domainTag index, make migrations

--- Infrastructure/Data/VaultonDbContext.cs
+++ Infrastructure/Data/VaultonDbContext.cs
@@ -2,128 +2,128 @@
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Data
+namespace Infrastructure.Data;
+
+public class VaultonDbContext(DbContextOptions<VaultonDbContext> options) : DbContext(options)
 {
-	public class VaultonDbContext(DbContextOptions<VaultonDbContext> options) : DbContext(options)
+	public DbSet<User> Users { get; set; } = default!;
+	public DbSet<Entry> Entries { get; set; } = default!;
+	public DbSet<RefreshToken> RefreshTokens { get; set; } = default!;
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
-		public DbSet<User> Users { get; set; }
-		public DbSet<Entry> Entries { get; set; }
-		public DbSet<RefreshToken> RefreshTokens { get; set; } = default!;
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		base.OnModelCreating(modelBuilder);
+
+		ConfigureEntries(modelBuilder);
+		ConfigureUsers(modelBuilder);
+		ConfigureRefreshTokens(modelBuilder);
+	}
+
+	private static void ConfigureEntries(ModelBuilder modelBuilder)
+	{
+		var b = modelBuilder.Entity<Entry>();
+
+		// Entry -> User
+		b.HasOne<User>()
+			.WithMany()
+			.HasForeignKey(e => e.UserId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		b.HasIndex(e => e.UserId);
+		b.HasIndex(e => e.DomainTag);
+		b.Property(e => e.DomainTag)
+			.HasMaxLength(CryptoSizes.DomainTagLen);
+
+		b.OwnsOne(e => e.Payload, p =>
 		{
-			modelBuilder.Entity<Entry>()
-				.HasOne<User>()
-				.WithMany()
-				.HasForeignKey(e => e.UserId)
-				.OnDelete(DeleteBehavior.Cascade);
+			p.Property(x => x.Nonce)
+				.HasColumnName("Payload_Nonce")
+				.HasMaxLength(CryptoSizes.GcmNonceLen)
+				.IsRequired();
 
-			modelBuilder.Entity<Entry>()
-				.HasIndex(e => e.UserId);
+			p.Property(x => x.Tag)
+				.HasColumnName("Payload_Tag")
+				.HasMaxLength(CryptoSizes.GcmTagLen)
+				.IsRequired();
 
-			modelBuilder.Entity<Entry>()
-				.OwnsOne(e => e.Payload, p =>
-				{
-					p.Property(x => x.Nonce)
-						.HasColumnName("Payload_Nonce")
-						.HasMaxLength(CryptoSizes.GcmNonceLen)
-						.IsRequired();
+			p.Property(x => x.CipherText)
+				.HasColumnName("Payload_CipherText")
+				.IsRequired();
+		});
 
-					p.Property(x => x.Tag)
-						.HasColumnName("Payload_Tag")
-						.HasMaxLength(CryptoSizes.GcmTagLen)
-						.IsRequired();
+		b.Navigation(e => e.Payload).IsRequired();
+	}
 
-					p.Property(x => x.CipherText)
-						.HasColumnName("Payload_CipherText")
-						.IsRequired();
-				});
+	private static void ConfigureUsers(ModelBuilder modelBuilder)
+	{
+		var b = modelBuilder.Entity<User>();
 
-			modelBuilder.Entity<User>()
-				.OwnsOne(u => u.MkWrapPwd, w =>
-				{
-					w.Property(x => x.Nonce)
-						.HasColumnName("MkWrapPwd_Nonce")
-						.HasMaxLength(CryptoSizes.GcmNonceLen)
-						.IsRequired();
+		b.Property(u => u.Verifier).HasMaxLength(CryptoSizes.VerifierLen);
+		b.Property(u => u.S_Verifier).HasMaxLength(CryptoSizes.SaltLen);
+		b.Property(u => u.S_Pwd).HasMaxLength(CryptoSizes.SaltLen);
 
-					w.Property(x => x.Tag)
-						.HasColumnName("MkWrapPwd_Tag")
-						.HasMaxLength(CryptoSizes.GcmTagLen)
-						.IsRequired();
+		b.Property(u => u.KdfMode).HasConversion<int>();
 
-					w.Property(x => x.CipherText)
-						.HasColumnName("MkWrapPwd_CipherText")
-						.HasMaxLength(CryptoSizes.MkLen)
-						.IsRequired();
-				});
+		b.OwnsOne(u => u.MkWrapPwd, w =>
+		{
+			w.Property(x => x.Nonce)
+				.HasColumnName("MkWrapPwd_Nonce")
+				.HasMaxLength(CryptoSizes.GcmNonceLen)
+				.IsRequired();
 
-			// Recovery Key is option for now
-			modelBuilder.Entity<User>()
-				.OwnsOne(u => u.MkWrapRk, w =>
-				{
-					w.Property(x => x.Nonce)
-						.HasColumnName("MkWrapRk_Nonce")
-						.HasMaxLength(CryptoSizes.GcmNonceLen)
-						.IsRequired(false);
+			w.Property(x => x.Tag)
+				.HasColumnName("MkWrapPwd_Tag")
+				.HasMaxLength(CryptoSizes.GcmTagLen)
+				.IsRequired();
 
-					w.Property(x => x.Tag)
-						.HasColumnName("MkWrapRk_Tag")
-						.HasMaxLength(CryptoSizes.GcmTagLen)
-						.IsRequired(false);
+			w.Property(x => x.CipherText)
+				.HasColumnName("MkWrapPwd_CipherText")
+				.HasMaxLength(CryptoSizes.MkLen)
+				.IsRequired();
+		});
 
-					w.Property(x => x.CipherText)
-						.HasColumnName("MkWrapRk_CipherText")
-						.HasMaxLength(CryptoSizes.MkLen)
-						.IsRequired(false);
-				});
-
-			modelBuilder.Entity<User>(b =>
-			{
-				b.Property(u => u.Verifier).HasMaxLength(CryptoSizes.VerifierLen);
-				b.Property(u => u.S_Verifier).HasMaxLength(CryptoSizes.SaltLen);
-				b.Property(u => u.S_Pwd).HasMaxLength(CryptoSizes.SaltLen);
-			});
-
-			modelBuilder.Entity<Entry>(b =>
-			{
-				b.Property(e => e.DomainTag).HasMaxLength(CryptoSizes.DomainTagLen);
-			});
-
-			modelBuilder.Entity<RefreshToken>(b =>
-			{
-				b.HasKey(rt => rt.Id);
-
-				b.HasOne(rt => rt.User)
-				 .WithMany()
-				 .HasForeignKey(rt => rt.UserId)
-				 .OnDelete(DeleteBehavior.Cascade);
-
-				b.Property(rt => rt.TokenHash)
-					.HasMaxLength(32)
-					.IsRequired();
-
-				b.HasIndex(rt => rt.TokenHash).IsUnique();
-				b.HasIndex(rt => rt.UserId);
-
-				b.Property(rt => rt.CreatedAt).IsRequired();
-				b.Property(rt => rt.ExpiresAt).IsRequired();
-			});
-
-			modelBuilder.Entity<User>()
-				.Navigation(u => u.MkWrapRk)
+		// MK wrap with recovery key (optional for now)
+		b.OwnsOne(u => u.MkWrapRk, w =>
+		{
+			w.Property(x => x.Nonce)
+				.HasColumnName("MkWrapRk_Nonce")
+				.HasMaxLength(CryptoSizes.GcmNonceLen)
 				.IsRequired(false);
 
-			modelBuilder.Entity<User>()
-				.Navigation(u => u.MkWrapPwd)
-				.IsRequired();
+			w.Property(x => x.Tag)
+				.HasColumnName("MkWrapRk_Tag")
+				.HasMaxLength(CryptoSizes.GcmTagLen)
+				.IsRequired(false);
 
-			modelBuilder.Entity<Entry>()
-				.Navigation(e => e.Payload)
-				.IsRequired();
+			w.Property(x => x.CipherText)
+				.HasColumnName("MkWrapRk_CipherText")
+				.HasMaxLength(CryptoSizes.MkLen)
+				.IsRequired(false);
+		});
 
-			modelBuilder.Entity<User>()
-				.Property(u => u.KdfMode)
-				.HasConversion<int>();
-		}
+		b.Navigation(u => u.MkWrapPwd).IsRequired();
+		b.Navigation(u => u.MkWrapRk).IsRequired(false);
+	}
+
+	private static void ConfigureRefreshTokens(ModelBuilder modelBuilder)
+	{
+		var b = modelBuilder.Entity<RefreshToken>();
+
+		b.HasKey(rt => rt.Id);
+
+		b.HasOne(rt => rt.User)
+			.WithMany()
+			.HasForeignKey(rt => rt.UserId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		b.Property(rt => rt.TokenHash)
+			.HasMaxLength(32)
+			.IsRequired();
+
+		b.HasIndex(rt => rt.TokenHash).IsUnique();
+		b.HasIndex(rt => rt.UserId);
+
+		b.Property(rt => rt.CreatedAt).IsRequired();
+		b.Property(rt => rt.ExpiresAt).IsRequired();
 	}
 }
